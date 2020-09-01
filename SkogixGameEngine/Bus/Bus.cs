4e03fa4dbaa593396a6a575c43178f5f5f0f7f25
Emil Skogsund <skogix@gmail.com>
2020-09-01 09:43:14 +0200

    basic hubpubsub

---
+++ SkogixGameEngine/Bus/Bus.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Bus
+{
+    public static class Hub
+    {
+        private static List<object> _bus = new List<object>();
+        private static List<Handler> _handlers = new List<Handler>();
+
+        public static void Sub<T>(object sub, Action<T> handler)
+        {
+            _handlers.Add(GetHandler<T>(sub, handler));
+        }
+        
+        private static Handler GetHandler<T>(object sub, Delegate handler)
+        {
+            return new Handler
+            {
+                Action = handler,
+                Type = typeof(T),
+                Sender = new WeakReference(sub)
+            };
+        }
+        
+        public static void Pub<T>(T data = default)
+        {
+            foreach (var handler in _handlers.Where(h => h.Type == typeof(T)))
+                if (handler.Action is Action<T> sendAction)
+                    sendAction(data);
+        }
+        
+    }
+    internal class Handler
+    {
+        public Delegate Action { get; set; }
+        public Type Type { get; set; }
+        public WeakReference Sender { get; set; }
+    }
+}
---
+++ SkogixGameEngine/ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Bus;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new object();
+            Hub.Sub<Skogix>(obj, onSkogix);
+            
+            
+            Hub.Pub(new Skogix("huhuhuhuu"));
+        }
+
+        private static void onSkogix(Skogix e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    internal class Skogix
+    {
+        public string Message;
+
+        public Skogix(string message)
+        {
+            Message = message;
+        }
+    }
+}
